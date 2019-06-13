348819b16482e0877657c11c5796a337be745fcf
Md Imteyaz Raza <imteyaz.raza@gmail.com>
2019-06-13 14:19:59 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,28 +7,3 @@ self.addEventListener('activate', e => {
     console.log("Activating Ferns N Petals screen widgit!");
     self.clients.claim();
 });
-self.addEventListener('fetch', async e => {
-    const req = e.request;
-    const url = new URL(req.url);
-    if (url.origin === location.origin) {
-        e.respondWith(cacheFirst(req));
-    } else {
-        e.respondWith(networkAndCache(req));
-    }
-});
-async function cacheFirst(req) {
-    const cache = await caches.open(cacheName);
-    const cached = await cache.match(req);
-    return cached || fetch(req);
-}
-async function networkAndCache(req) {
-    const cache = await caches.open(cacheName);
-    try {
-        const fresh = await fetch(req);
-        await cache.put(req, fresh.clone());
-        return fresh;
-    } catch (e) {
-        const cached = await cache.match(req);
-        return cached;
-    }
-}
