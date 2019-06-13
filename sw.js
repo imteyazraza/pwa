5a0fe09cc2693f5fb33ee5fa03dfbc83449fb58e
Md Imteyaz Raza <imteyaz.raza@gmail.com>
2019-06-13 14:14:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,34 @@
-const cacheName='news-v1';const staticAssets=[];self.addEventListener('install',async e=>{const cache=await caches.open(cacheName);await cache.addAll(staticAssets);return self.skipWaiting();});self.addEventListener('activate',e=>{self.clients.claim();});self.addEventListener('fetch',async e=>{const req=e.request;const url=new URL(req.url);if(url.origin===location.origin){e.respondWith(cacheFirst(req));}else{e.respondWith(networkAndCache(req));}});async function cacheFirst(req){const cache=await caches.open(cacheName);const cached=await cache.match(req);return cached||fetch(req);}
-async function networkAndCache(req){const cache=await caches.open(cacheName);try{const fresh=await fetch(req);await cache.put(req,fresh.clone());return fresh;}catch(e){const cached=await cache.match(req);return cached;}}
+const cacheName = 'news-v1';
+self.addEventListener('install', async e => {
+    console.log("Thanks for adding Ferns N Petals to your home screen!");
+    return self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+    console.log("Activating Ferns N Petals screen widgit!");
+    self.clients.claim();
+});
+self.addEventListener('fetch', async e => {
+    const req = e.request;
+    const url = new URL(req.url);
+    if (url.origin === location.origin) {
+        e.respondWith(cacheFirst(req));
+    } else {
+        e.respondWith(networkAndCache(req));
+    }
+});
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cached = await cache.match(req);
+    return cached || fetch(req);
+}
+async function networkAndCache(req) {
+    const cache = await caches.open(cacheName);
+    try {
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
+    } catch (e) {
+        const cached = await cache.match(req);
+        return cached;
+    }
+}
