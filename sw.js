64f6424f2dca078e73cdf14f674d73cd07647b7e
Md Imteyaz Raza <imteyaz.raza@gmail.com>
2019-06-13 14:32:01 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,3 +16,19 @@ self.addEventListener('fetch', async e => {
         e.respondWith(networkAndCache(req));
     }
 });
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cached = await cache.match(req);
+    return cached || fetch(req);
+}
+async function networkAndCache(req) {
+    const cache = await caches.open(cacheName);
+    try {
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
+    } catch (e) {
+        const cached = await cache.match(req);
+        return cached;
+    }
+}
