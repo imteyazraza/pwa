50002e6102f0f4f64cbcdb41c298d72a42759a97
Md Imteyaz Raza <imteyaz.raza@gmail.com>
2019-06-20 12:24:13 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,17 @@
-
+const cacheName = 'news-v1';
+self.addEventListener('install', async e => {
+    
+    return self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+    self.clients.claim();
+});
+self.addEventListener('fetch', async e => {
+    const req = e.request;
+    const url = new URL(req.url);
+    if (url.origin === location.origin) {
+        
+    } else {
+       
+    }
+});
