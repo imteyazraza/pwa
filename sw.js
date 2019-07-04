0068c267c8b8016626ba8035fe41fdcd31c4b2eb
Md Imteyaz Raza <imteyaz.raza@gmail.com>
2019-07-04 07:02:30 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,11 @@
 console.log('self>>>>',self)
-let str = self.location.pathname.split('/')[2];
+let str = self.location.search;
 console.log('str',str);
+
+let urlParams = new URLSearchParams(self.location.search);
+let myParam = urlParams.get('myParam');
+console.log('myParam',myParam);
+
 if(str != 'about.html'){
 self.addEventListener('install', async e => {
     
